57b0af19c8cf449055cedfacfd06b74e1eda4079
pajuhy123 <pajuhy123@gmail.com>
2019-04-29 20:21:53 +0900

    version up test

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # printtt_test
+
+pip install setuptools
+pip install wheel
+python setup.py bdist_wheel
+
+twine upload dist/pyquibase-1.0-py3-none-any.whl
---
+++ setup.py
@@ -0,0 +1,22 @@
+#from distutils.core import setup
+from setuptools import setup
+
+
+
+setup(
+  name = 'printtt_test',
+  packages = ['printtt_test'], # this must be the same as the name above
+  version = '0.0.2',
+  description = 'testing printtt_test',
+  #long_description=long_description,
+  author = 'someone',
+  #author_email = 'kbpark.linguist@gmail.com',
+  url = 'https://github.com/pajuhy123/printtt_test', # use the URL to the github repo
+  download_url = 'https://github.com/pajuhy123/printtt_test.git', # I'll explain this in a second
+  keywords = ['print'], # arbitrary keywords
+  classifiers = [],
+  #install_requires = [
+  #],
+  #license='',
+  #include_package_data=True
+)
